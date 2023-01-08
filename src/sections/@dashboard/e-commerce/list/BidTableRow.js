c715ce95de5183d9918c4247b590f2ca62cbdf38
helmy162 <104876574+helmy162@users.noreply.github.com>
2023-01-09 00:31:27 +0200

    product table

---
+++ src/sections/@dashboard/e-commerce/list/BidTableRow.js
@@ -0,0 +1,108 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import { sentenceCase } from 'change-case';
+// @mui
+import {
+  Stack,
+  Button,
+  TableRow,
+  Checkbox,
+  MenuItem,
+  TableCell,
+  IconButton,
+  Link,
+} from '@mui/material';
+// utils
+import { fDate } from '../../../../utils/formatTime';
+import { fCurrency } from '../../../../utils/formatNumber';
+// components
+import Label from '../../../../components/label';
+import Image from '../../../../components/image';
+import Iconify from '../../../../components/iconify';
+import MenuPopover from '../../../../components/menu-popover';
+import ConfirmDialog from '../../../../components/confirm-dialog';
+
+// ----------------------------------------------------------------------
+
+ProductTableRow.propTypes = {
+  row: PropTypes.object,
+  selected: PropTypes.bool,
+  onEditRow: PropTypes.func,
+  onViewRow: PropTypes.func,
+  onSelectRow: PropTypes.func,
+  onDeleteRow: PropTypes.func,
+};
+
+export default function ProductTableRow({
+  row,
+  selected,
+  onSelectRow,
+  onDeleteRow,
+  onEditRow,
+  onViewRow,
+}) {
+  const { id, name, time, bid} = row;
+
+  const [openConfirm, setOpenConfirm] = useState(false);
+
+  const [openPopover, setOpenPopover] = useState(null);
+
+  const handleOpenConfirm = () => {
+    setOpenConfirm(true);
+  };
+
+  const handleCloseConfirm = () => {
+    setOpenConfirm(false);
+  };
+
+  const handleOpenPopover = (event) => {
+    setOpenPopover(event.currentTarget);
+  };
+
+  const handleClosePopover = () => {
+    setOpenPopover(null);
+  };
+
+  return (
+    <>
+      <TableRow hover selected={selected}>
+        
+
+        <TableCell>
+          <Stack direction="row" alignItems="center" spacing={2}>
+
+            <Link
+              noWrap
+              color="inherit"
+              variant="subtitle2"
+              onClick={onViewRow}
+              sx={{ cursor: 'pointer' }}
+            >
+              #{id}
+            </Link>
+          </Stack>
+        </TableCell>
+        <TableCell>{name}</TableCell>
+        <TableCell>{time}</TableCell>
+        <TableCell>{bid}</TableCell>
+        <TableCell padding="checkbox">
+          <Checkbox checked={selected} onClick={onSelectRow} />
+        </TableCell>
+
+      </TableRow>
+
+
+      <ConfirmDialog
+        open={openConfirm}
+        onClose={handleCloseConfirm}
+        title="Delete"
+        content="Are you sure want to delete?"
+        action={
+          <Button variant="contained" color="error" onClick={onDeleteRow}>
+            Delete
+          </Button>
+        }
+      />
+    </>
+  );
+}
---
+++ src/sections/@dashboard/e-commerce/list/ProductTableRow.js
@@ -0,0 +1,154 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import { sentenceCase } from 'change-case';
+// @mui
+import {
+  Stack,
+  Button,
+  TableRow,
+  Checkbox,
+  MenuItem,
+  TableCell,
+  IconButton,
+  Link,
+} from '@mui/material';
+// utils
+import { fDate } from '../../../../utils/formatTime';
+import { fCurrency } from '../../../../utils/formatNumber';
+// components
+import Label from '../../../../components/label';
+import Image from '../../../../components/image';
+import Iconify from '../../../../components/iconify';
+import MenuPopover from '../../../../components/menu-popover';
+import ConfirmDialog from '../../../../components/confirm-dialog';
+
+// ----------------------------------------------------------------------
+
+ProductTableRow.propTypes = {
+  row: PropTypes.object,
+  selected: PropTypes.bool,
+  onEditRow: PropTypes.func,
+  onViewRow: PropTypes.func,
+  onSelectRow: PropTypes.func,
+  onDeleteRow: PropTypes.func,
+};
+
+export default function ProductTableRow({
+  row,
+  selected,
+  onSelectRow,
+  onDeleteRow,
+  onEditRow,
+  onViewRow,
+}) {
+  const { id, make, model, year, seller_name, auction } = row;
+
+  const [openConfirm, setOpenConfirm] = useState(false);
+
+  const [openPopover, setOpenPopover] = useState(null);
+
+  const handleOpenConfirm = () => {
+    setOpenConfirm(true);
+  };
+
+  const handleCloseConfirm = () => {
+    setOpenConfirm(false);
+  };
+
+  const handleOpenPopover = (event) => {
+    setOpenPopover(event.currentTarget);
+  };
+
+  const handleClosePopover = () => {
+    setOpenPopover(null);
+  };
+
+  return (
+    <>
+      <TableRow hover selected={selected}>
+        <TableCell padding="checkbox">
+          <Checkbox checked={selected} onClick={onSelectRow} />
+        </TableCell>
+
+        <TableCell>
+          <Stack direction="row" alignItems="center" spacing={2}>
+
+            <Link
+              noWrap
+              color="inherit"
+              variant="subtitle2"
+              onClick={onViewRow}
+              sx={{ cursor: 'pointer' }}
+            >
+              #{id}
+            </Link>
+          </Stack>
+        </TableCell>
+        <TableCell>{make}</TableCell>
+        <TableCell>{model}</TableCell>
+        <TableCell>{year}</TableCell>
+        <TableCell>{seller_name}</TableCell>
+
+        <TableCell align="center">
+          <Label
+            variant="soft"
+            color={
+              (auction === 'expired' && 'error') ||
+              (auction === 'wait_auction' && 'warning') ||
+              (auction === 'active' && 'success') || 'success'
+            }
+            sx={{ textTransform: 'capitalize', minWidth:'100px'}}
+          >
+            {auction ? sentenceCase(auction) : ''}
+          </Label>
+        </TableCell>
+
+        <TableCell align="right">
+          <IconButton color={openPopover ? 'primary' : 'default'} onClick={handleOpenPopover}>
+            <Iconify icon="eva:more-vertical-fill" />
+          </IconButton>
+        </TableCell>
+      </TableRow>
+
+      <MenuPopover
+        open={openPopover}
+        onClose={handleClosePopover}
+        arrow="right-top"
+        sx={{ width: 140 }}
+      >
+        <MenuItem
+          onClick={() => {
+            handleOpenConfirm();
+            handleClosePopover();
+          }}
+          sx={{ color: 'error.main' }}
+        >
+          <Iconify icon="eva:trash-2-outline" />
+          Delete
+        </MenuItem>
+
+        <MenuItem
+          onClick={() => {
+            onEditRow();
+            handleClosePopover();
+          }}
+        >
+          <Iconify icon="eva:edit-fill" />
+          Edit
+        </MenuItem>
+      </MenuPopover>
+
+      <ConfirmDialog
+        open={openConfirm}
+        onClose={handleCloseConfirm}
+        title="Delete"
+        content="Are you sure want to delete?"
+        action={
+          <Button variant="contained" color="error" onClick={onDeleteRow}>
+            Delete
+          </Button>
+        }
+      />
+    </>
+  );
+}
