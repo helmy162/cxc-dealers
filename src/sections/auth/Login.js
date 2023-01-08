c4c7783e479ead862e9e2d8d68e7093b9ef2887b
helmy162 <104876574+helmy162@users.noreply.github.com>
2023-01-09 00:33:44 +0200

    login page enhancements

--- src/sections/auth/Login.js
+++ src/sections/auth/Login.js
@@ -16,22 +16,13 @@ export default function Login() {
   return (
     <LoginLayout>
       <Stack spacing={2} sx={{ mb: 5, position: 'relative' }}>
-        <Typography variant="h4">Sign in to Minimal</Typography>
+        <Typography variant="h4">Sign in</Typography>
 
         <Stack direction="row" spacing={0.5}>
           <Typography variant="body2">New user?</Typography>
 
           <Link variant="subtitle2" href={PATH_AUTH.register}>Create an account</Link>
         </Stack>
-
-        <Tooltip title={method} placement="left">
-          <Box
-            component="img"
-            alt={method}
-            src={`/assets/icons/auth/ic_${method}.png`}
-            sx={{ width: 32, height: 32, position: 'absolute', right: 0 }}
-          />
-        </Tooltip>
       </Stack>
 
       <AuthLoginForm />
