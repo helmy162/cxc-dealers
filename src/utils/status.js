a10b8fbcf877a208522a95e6ae5e6116ebd62f25
helmy162 <104876574+helmy162@users.noreply.github.com>
2023-02-11 01:12:00 +0200

    added car status & timer

---
+++ src/utils/status.js
@@ -0,0 +1,41 @@
+
+export function carStatus( car ) {
+    
+    let liveStatus;
+    const endDate = new Date(car?.auction?.end_at);
+    const startDate = new Date(car?.auction?.start_at);
+    const currentTime = new Date();
+    const difference = endDate - currentTime;
+    if(car?.status?.toLocaleLowerCase() !== 'approved'){
+        liveStatus = 'pending';
+    }
+    else if(currentTime < startDate) {
+        liveStatus = 'upcoming';
+    }
+    else if (difference < 0) {
+        liveStatus = 'expired';
+    }
+    else{
+        liveStatus = 'live';
+    }
+    
+    
+
+    return liveStatus;
+    
+}
+
+export function carTimer( productStatus ) {
+    let timeRemaining;
+        if (productStatus < 0 || productStatus === null) {
+            timeRemaining = '';
+            return;
+        }
+        const hours = Math.floor(productStatus / 1000 / 60 / 60);
+        const minutes = Math.floor((productStatus / 1000 / 60) % 60);
+        const seconds = Math.floor((productStatus / 1000) % 60);
+        timeRemaining = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+
+    return timeRemaining;
+
+}
