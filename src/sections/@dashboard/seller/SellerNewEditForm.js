8836314a8e629032980f2273440d3372b18aacfa
helmy162 <104876574+helmy162@users.noreply.github.com>
2023-02-17 23:24:06 +0200

    Add/Edit Seller Form

---
+++ src/sections/@dashboard/seller/SellerNewEditForm.js
@@ -0,0 +1,138 @@
+import PropTypes from 'prop-types';
+import * as Yup from 'yup';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+// form
+import { useForm, Controller } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+// @mui
+import { LoadingButton } from '@mui/lab';
+import { Box, Card, Grid, Stack, Switch, Typography, FormControlLabel } from '@mui/material';
+// utils
+import { fData } from '../../../utils/formatNumber';
+// routes
+import { PATH_DASHBOARD } from '../../../routes/paths';
+// assets
+import { countries } from '../../../assets/data';
+// components
+import Label from '../../../components/label';
+import { useSnackbar } from '../../../components/snackbar';
+import FormProvider, {
+  RHFSelect,
+  RHFSwitch,
+  RHFTextField,
+  RHFUploadAvatar,
+} from '../../../components/hook-form';
+
+import axiosInstance from 'src/utils/axios';
+// ----------------------------------------------------------------------
+
+SellerNewEditForm.propTypes = {
+  isEdit: PropTypes.bool,
+  currentUser: PropTypes.object,
+};
+
+export default function SellerNewEditForm({ isEdit = false, currentUser }) {
+  const navigate = useNavigate();
+
+  const { enqueueSnackbar } = useSnackbar();
+
+  const NewUserSchema = Yup.object().shape({
+    name: Yup.string().required('Name is required'),
+    email: Yup.string().required('Email is required').email('Email must be a valid email address'),
+    phone: Yup.string().required('Phone is required'),
+  });
+
+  const defaultValues = useMemo(
+    () => ({
+      name: currentUser?.name || '',
+      email: currentUser?.email || '',
+      phone: currentUser?.phone || '',
+    }),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [currentUser]
+  );
+
+  const methods = useForm({
+    resolver: yupResolver(NewUserSchema),
+    defaultValues,
+  });
+
+  const {
+    reset,
+    watch,
+    control,
+    setValue,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = methods;
+
+  const values = watch();
+
+  useEffect(() => {
+    if (isEdit && currentUser) {
+      reset(defaultValues);
+    }
+    if (!isEdit) {
+      reset(defaultValues);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isEdit, currentUser]);
+
+  const onSubmit = async (data) => {
+    try {
+      let res;
+      isEdit?  res = await axiosInstance.put(`admin/sellers/${currentUser.id}`, data) :  res = await axiosInstance.post('admin/sellers', data);
+      enqueueSnackbar(!isEdit ? 'Create success!' : 'Update success!');
+      navigate(PATH_DASHBOARD.seller.list);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleDrop = useCallback(
+    (acceptedFiles) => {
+      const file = acceptedFiles[0];
+
+      const newFile = Object.assign(file, {
+        preview: URL.createObjectURL(file),
+      });
+
+      if (file) {
+        setValue('avatarUrl', newFile, { shouldValidate: true });
+      }
+    },
+    [setValue]
+  );
+
+  return (
+    <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+      <Grid container spacing={3}>
+
+        <Grid item xs={12} md={12}>
+          <Card sx={{ p: 3 }}>
+            <Box
+              rowGap={3}
+              columnGap={2}
+              display="grid"
+              gridTemplateColumns={{
+                xs: 'repeat(1, 1fr)',
+                sm: 'repeat(1, 1fr)',
+              }}
+            >
+              <RHFTextField name="name" label="Full Name"/>
+              <RHFTextField name="email" label="Email Address"/>
+              <RHFTextField name="phone" label="Phone Number"/>
+            </Box>
+
+            <Stack alignItems="flex-end" sx={{ mt: 3 }}>
+              <LoadingButton type="submit" variant="contained" loading={isSubmitting}>
+                {!isEdit ? 'Create Seller' : 'Save Changes'}
+              </LoadingButton>
+            </Stack>
+          </Card>
+        </Grid>
+      </Grid>
+    </FormProvider>
+  );
+}
